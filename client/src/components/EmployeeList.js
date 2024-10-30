6f982a640fb114490d855e95cec18c34ad23ee45
uehara <uehara.azusa0827@gmail.com>
2024-10-30 14:31:02 +0900

    add:フィードバック内容の表示

--- client/src/components/EmployeeList.js
+++ client/src/components/EmployeeList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import employeeService from "../services/employeeService";
 
 const EmployeeList = () => {
@@ -7,13 +7,15 @@ const EmployeeList = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // employeeService.jsを通じてAPIリクエストを送る
-    employeeService.getEmployee()
+    //employeeService.jsを通じてAPIリクエストを送る
+    employeeService
+      .getEmployee()
       .then((response) => setEmployee(response.data))
       .catch((error) => console.error("社員情報の取得に失敗しました：", error));
   }, []);
 
   return (
+    //社員情報一覧リストの表示設定
     <table border="1" className="employeelist">
       <tr>
         <th>ID</th>
@@ -22,18 +24,25 @@ const EmployeeList = () => {
         <th>情報共有ボタン</th>
       </tr>
       {employee.map((val) => (
-          <tr key={val.id}>
-            <td>{val.id}</td>
-            <td>
-            {val.name_kana}<br />
+        <tr key={val.id}>
+          <td>{val.id}</td>
+          <td>
+            {val.name_kana}
+            <br />
             {val.name_kanji}
-            </td>
-            <td>
-            {val.sales_kana}<br />
+          </td>
+          <td>
+            {val.sales_kana}
+            <br />
             {val.sales_kanji}
-            </td>
-          <td><button onClick={() => navigate(`/detail/${val.id}`)}>共有情報</button></td>
-          </tr>
+          </td>
+          {/* URLの最後にデータベースの社員IDの数字を付与してそれぞれの情報とリンクさせる */}
+          <td>
+            <button onClick={() => navigate(`/detail/${val.id}`)}>
+              共有情報
+            </button>
+          </td>
+        </tr>
       ))}
     </table>
   );
